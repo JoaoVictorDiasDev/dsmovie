2a14d618cf0a9dba4baa0bd3bc435548fc4575f0
Joao Dias <joaovictordiasbandini@gmail.com>
2022-01-16 15:45:01 -0300

    Debug Movie Listing

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -3,7 +3,7 @@ import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { BASE_URL } from "utils/requests";
-import { Movie, MoviePage } from "types/movie";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
